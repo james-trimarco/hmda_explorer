a24bb68b228268711f54cfd848f4602f130e7ba5
James Trimarco <james.trimarco@gmail.com>
2017-10-25 10:08:50 -0700

    renamed HTML file and updated code

--- scripts/listCleanUp.js
+++ scripts/listCleanUp.js
@@ -1,16 +1,20 @@
-
 var url = 'https://api.consumerfinance.gov:443/data/hmda/slice/hmda_lar.json?$select=msamd,msamd_name&$group=msamd,msamd_name&$orderBy=msamd,msamd_name&$limit=1000&$format=json'
 
 var metroData = $.ajax({
         accept: 'application/json',
         method: "GET",
         datatype: 'json',
-        url: url, // Passes the dynamically built URL to the ajax call.
-        timeout: 30000
+        url: url,
+        timeout: 10000
     })
     .done(function (json) {
         console.log("success!");
-        })
+
+        var clean = cleanUp(json.results);
+        var unitedStatesTemplate = buildStateList(clean);
+        var myJson = parseCleanedList(clean, unitedStatesTemplate);
+        console.log(JSON.stringify(myJson));
+    })
     .fail(function (jqXHR, textStatus, errorThrown) {
         console.log("error=" + errorThrown);
     });
@@ -24,14 +28,16 @@ function cleanUp(json) {
 
     for (i = 0; i < json.length; i++) {
         json[i].states = [];
-        var pieces = json[i].msamd_name.split(' - ');
-        json[i].msamd_name = pieces[0];
-        var stateNames = pieces[1].split(',');
+        if (json[i].msamd_name) {
+            var pieces = json[i].msamd_name.split(' - ');
+            json[i].msamd_name = pieces[0];
+            var stateNames = pieces[1].split(',');
 
-        for (j = 0; j < stateNames.length; j++) {
-            var state = stateNames[j].trim();
-            if (state.length === 2) {
-                json[i].states.push(state);
+            for (j = 0; j < stateNames.length; j++) {
+                var state = stateNames[j].trim();
+                if (state.length === 2) {
+                    json[i].states.push(state);
+                }
             }
         }
     }
@@ -43,7 +49,9 @@ function buildStateList(json) {
         j,
         k,
         stateTracker = [], // This is just a counter
-        unitedStates = []; // Here's the object that will give us a nested array of states with metro areas inside.
+        unitedStates = {
+            states: []
+        }; // Here's the object that will give us a nested array of states with metro areas inside.
 
     for (i = 0; i < json.length; i++) {
         for (j = 0; j < json[i].states.length; j++) {
@@ -56,42 +64,34 @@ function buildStateList(json) {
 
     for (k = 0; k < stateTracker.length; k++) {
         var obj = {
-            state: stateTracker[k],
+            state_abbr: stateTracker[k],
             cities: []
         };
-            unitedStates.push(obj);
+        unitedStates.states.push(obj);
     }
     return unitedStates;
 }
 
 function parseCleanedList(list, template) {
-
     var i,
         j,
         k;
     for (i = 0; i < list.length; i++)
         for (j = 0; j < list[i].states.length; j++) {
             var st = list[i].states[j];
-            //console.log(st);
 
-            for (k = 0; k < template.length; k++) {
-                if (st === template[k].state) {
-                    //console.log(list[i].msamd);
+            for (k = 0; k < template.states.length; k++) {
+            console.log("check");
+
+                if (st === template.states[k].state_abbr) {
+
                     var obj = {
-                        msamd_name: list[i].msamd_name,
-                        msamd: list[i].msamd
+                        metro_name: list[i].msamd_name,
+                        metro_number: list[i].msamd
                     };
-                    template[k].cities.push(obj);
+                    template.states[k].cities.push(obj);
                 }
             }
         }
-console.log(JSON.stringify(template));
+    return template;
 }
-
-var clean = cleanUp(metroData);
-var unitedStatesTemplate = buildStateList(clean);
-parseCleanedList(clean, unitedStatesTemplate);
-
-
-
-//console.log(JSON.stringify(newMetroList));
